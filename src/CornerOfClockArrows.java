6da9f7cb478f5855aa48b3df08c83f64ecfc2258
soufee <soufee@mail.ru>
2018-07-26 16:23:30 +0300

    FindCorner

--- src/CornerOfClockArrows.java
+++ src/CornerOfClockArrows.java
@@ -1,18 +1,17 @@
 public class CornerOfClockArrows {
 
     public static double getCorner(double hours, double mins) {
-        if (hours < 0 || hours > 24) return -1;
-        if (mins < 0 || mins > 60) return -1;
+        if (hours < 0 || hours > 24) throw new IllegalStateException("Введено неверное значение");
+        if (mins < 0 || mins > 60) throw new IllegalStateException("Введено неверное значение");
         hours = 360 / 12 * (hours >= 12 ? hours - 12 : hours);
         mins = 360 / 12 * (mins / 5);
-        System.out.println("hours = " + hours + "; mins = " + mins);
-        return Math.abs(mins - hours);
+        double res = Math.abs(mins - hours);
+        return res>180?res-180:res;
     }
 
     public static void main(String[] args) {
-        System.out.println(test1());
-        System.out.println(test2());
-        System.out.println(test3());
+
+        System.out.println(getCorner(15, 19));
     }
 
     private static double test1() {
